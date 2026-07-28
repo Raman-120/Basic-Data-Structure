399b25c18643099970ca6d4efe1daf7e52b2755f
Raman-120 <ramanchaulagain059@gmail.com>
2026-07-28 22:13:50 +0545

    Queue and how to use them

--- Basic Data structures/src/Queues.java
+++ Basic Data structures/src/Queues.java
@@ -1,10 +1,33 @@
-
 import java.util.LinkedList;
 import java.util.Queue;
 
 public class Queues {
     public static void main(String[] args){
 
+        // add = enqueue , offer()
+        // remove = dequeue ,poll()
+        // element() or peek() method is used to look at the head of the queue or the first element of the queue
+
         Queue<String> queue = new LinkedList<>();
+
+        // adding elements in the queue
+        queue.offer("The Police");
+        queue.offer("Guns N Roses");
+        queue.offer("Metallica");
+        queue.offer("Axix Band");
+
+        //displaying which element is the first element in  queue
+        System.out.println(queue.element()); //displays The Police
+
+        //displaying queue
+        System.out.println(queue);
+
+        // removing elements in the queue
+        queue.poll();
+
+        System.out.println(queue);
+
+        //checks if the object is available in the queue
+        System.out.println(queue.contains("Guns N Roses"));
     }
 }
