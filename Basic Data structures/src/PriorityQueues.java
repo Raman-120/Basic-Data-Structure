a018173326f3cafc323d066e67d5964c2640f5a0
Raman-120 <ramanchaulagain059@gmail.com>
2026-07-29 21:23:44 +0545

    priority queue in String data type

--- Basic Data structures/src/PriorityQueues.java
+++ Basic Data structures/src/PriorityQueues.java
@@ -6,12 +6,19 @@ public class PriorityQueues {
     // Priority Queue is used to serve elements
     // Higher priority queue are served first before lower priority
 
-        Queue<Double> priorityQueue = new PriorityQueue<>(Collections.reverseOrder());
-        priorityQueue.offer(3.0);
-        priorityQueue.offer(4.0);
-        priorityQueue.offer(1.5);
-        priorityQueue.offer(3.6);
-        priorityQueue.offer(2.8);
+//        Queue<Double> priorityQueue = new PriorityQueue<>(Collections.reverseOrder());
+//        priorityQueue.offer(3.0);
+//        priorityQueue.offer(4.0);
+//        priorityQueue.offer(1.5);
+//        priorityQueue.offer(3.6);
+//        priorityQueue.offer(2.8);
+
+
+        Queue<String> priorityQueue = new PriorityQueue<>(Collections.reverseOrder());
+        priorityQueue.offer("A");
+        priorityQueue.offer("C");
+        priorityQueue.offer("F");
+        priorityQueue.offer("B");
 
         while(!priorityQueue.isEmpty()){
             System.out.println(priorityQueue.poll());
