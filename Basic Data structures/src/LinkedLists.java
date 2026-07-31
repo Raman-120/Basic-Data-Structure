3106bfae82e384a07e1c4eb60b7e0113068dd50e
Raman-120 <ramanchaulagain059@gmail.com>
2026-07-31 23:16:46 +0545

    insertion, deletion, searching and Peeking the elements in LinkedList

--- Basic Data structures/src/LinkedLists.java
+++ Basic Data structures/src/LinkedLists.java
@@ -23,9 +23,36 @@ public class LinkedLists {
         linkedList.offer("Violet");
         linkedList.offer("Drum");
         linkedList.offer("Bass");
-        linkedList.poll(); // removes the first object as Queue follow FIFO  i.e. Guitar will be removed
+        //linkedList.poll(); // removes the first object as Queue follow FIFO  i.e. Guitar will be removed
+
+        // LinkedLists really shines in insertion and deletion
+
+        //For insertion
+        linkedList.add(3,"Bansuri");
+
+        //For deletion
+        linkedList.remove("Violet");
+
+        // We can also use LinkedList for search but that is not recommended as it is non-contiguous i.e.
+        // it stores data at anywhere in the computer memory location
+
+        //For Searching an element
+        System.out.println(linkedList.indexOf("Drum"));
+
+
+        // We can also peek the head or tail element in LinkedList
+        System.out.println(linkedList.peekFirst()); // For head peek
+        System.out.println(linkedList.peekLast()); // For tail peek
+
+        // We can also add and remove element at the head or tail of the LinkedList
+        linkedList.addFirst("Sitar"); // Add at the first index
+        linkedList.addLast("Madal"); // Add at the last index
+
+        linkedList.removeFirst(); // Removes the first element
+        linkedList.removeLast(); // Removes the last element
 
         System.out.println(linkedList);
+
 
 
     }
