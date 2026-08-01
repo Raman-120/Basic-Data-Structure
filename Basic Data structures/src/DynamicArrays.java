3b11581d628df0445db055ee2ef3fc6e6f12c939
Raman-120 <ramanchaulagain059@gmail.com>
2026-08-01 09:33:04 +0545

    added add method,insertion method, isEmpty method and toString method

--- Basic Data structures/src/DynamicArrays.java
+++ Basic Data structures/src/DynamicArrays.java
@@ -1,7 +1,7 @@
 public class DynamicArrays {
 
-    int size;
-    int capacity = 10;
+    int size; // it shows array size
+    int capacity = 10; // it shows arrays capacity
     Object[] array;
 
     DynamicArrays(){
@@ -13,6 +13,59 @@ public class DynamicArrays {
         this.array = new Object[capacity];
     }
 
+    // common used methods
+    public void add(Object object){
+        if(size >= capacity){
+            grow();
+        }
+        array[size] = object;
+        size++;
+    }
 
+    public void insert(int index, Object object){
+        if(size >= capacity){
+            grow();
+        }
+        for(int i = size; i > index; i--){
+            array[i] = array[i-1]; // [i-1] shifts all elements over to the right to make room for insertion.
+        }
+        array[index] = object;
+        size++;
+    }
+
+    public void delete(Object object){
+
+    }
+
+    public int search(){
+        return 1;
+    }
+
+    public void grow(){
+
+    }
+
+    public void shrink(){
+
+    }
+
+    public boolean isEmpty(){
+        return size == 0;
+    }
+
+    public String toString(){
+        String string = "";
+        for(int i = 0; i < size; i++){
+            string += array[i] + ", ";
+        }
+        if (string != "") {
+            string = "[" + string.substring(0, string.length() -2) + "]";
+        }
+        else{
+            string = "[]";
+        }
+
+        return string;
+    }
 
 }
--- Basic Data structures/src/DynamicArraysDemo.java
+++ Basic Data structures/src/DynamicArraysDemo.java
@@ -1,8 +1,19 @@
 public class DynamicArraysDemo {
     public static void main(String[] args){
 
-        DynamicArrays dynamicArrays = new DynamicArrays(15);
-        System.out.println(dynamicArrays.capacity);
+        DynamicArrays dynamicArrays = new DynamicArrays();
+
+        dynamicArrays.add("Guitar");
+        dynamicArrays.add(4);
+        dynamicArrays.add(true);
+
+        dynamicArrays.insert(0, 3.6);
+
+//        System.out.println(dynamicArrays.isEmpty());
+//        System.out.println(dynamicArrays.size);
+//        System.out.println(dynamicArrays.capacity);
+        System.out.println(dynamicArrays);
+
 
     }
 }
