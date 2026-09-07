2f507ed04b7e2664fbece79e464b64cf89079d14
Raman-120 <ramanchaulagain059@gmail.com>
2026-09-07 22:13:18 +0545

    implementing custom binary search method

--- Basic Data structures/src/BinarySearch.java
+++ Basic Data structures/src/BinarySearch.java
@@ -1,4 +1,5 @@
 import java.util.Arrays;
+import java.lang.Math;
 
 public class BinarySearch {
     public static void main(String[] args){
@@ -14,16 +15,52 @@ public class BinarySearch {
         for(int i = 0; i < array.length; i++){
             array[i] = i;
         }
+//
+//        int index = Arrays.binarySearch(array, target);
+//
+//        if(index == -1){
+//            System.out.println("Element not found.");
+//        }
+//        else{
+//            System.out.println("Element found at index: " + index);
+//        }
 
-        int index = Arrays.binarySearch(array, target);
+
+        // This code snippet is to understand how binary search works
+
+
+        int index = binarySearch(array, target);
 
         if(index == -1){
             System.out.println("Element not found.");
-        }
-        else{
+        } else {
             System.out.println("Element found at index: " + index);
         }
 
+    }
 
+    private static int binarySearch(int[] array, int target){
+
+        int low = 0;
+        int high = array.length - 1;
+
+        while(low <= high){
+            int middle  = low + (high - low) / 2;
+            int value = array[middle];
+
+            System.out.println(value); // this is optional, you can see the steps and the value you will get
+            // during bisection.
+            if(value < target){
+                low = middle +1;
+            }
+            else if(value > target){
+                high = middle - 1;
+            }
+            else {
+                return  middle; // target found.
+            }
+        }
+
+        return -1;
     }
 }
